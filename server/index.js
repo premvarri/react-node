b6c622e55f32fe5d889535067ffe7738bde6da78
prem varri <nvarri@miraclesoft.com>
2022-01-07 20:37:58 +0530

    Master trigger changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.get("/api/v1", (req, res) => {
-  res.json({ message: "Hello from NYC" });
+  res.json({ message: "Master Trigger API" });
 });
 app.get("*", function (req, res) {
   res.redirect("/");
