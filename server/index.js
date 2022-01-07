83d84120301b0c2686aae24d0b103eb7f221a8bc
prem varri <nvarri@miraclesoft.com>
2022-01-08 03:28:12 +0530

    production final deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.get("/api/v1", (req, res) => {
+  console.log("final master API")
   res.json({ message: "Master production final Trigger API" });
 });
 app.get("*", function (req, res) {
